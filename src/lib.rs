1d457a0b3bdafe5a0387b488735cfe524a678181
Jon Gjengset <jon@thesquareplanet.com>
2019-07-16 09:08:18 -0400

    Allow overriding environment vars for test run

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,12 @@ fn opt_out_test() {}
 #[test]
 fn always_test() {}
 
+#[cfg(test)]
+#[test]
+fn require_env() {
+    std::env::var("ENV_IS_SET").unwrap();
+}
+
 // check that minrust gets set correctly
 #[allow(unused_imports)]
 use std::sync::atomic::AtomicU64;
